6e4e68d34d580e12224a27c15959f77d3981303b
jiangqiqian <jiangqiqian@sequoiadb.com>
2024-05-16 08:49:14 +0000

    初始化脚本

---
+++ changeIP.js
@@ -0,0 +1,57 @@
+import('config.js');
+
+function checkArgs() {
+    try {
+        var db = new Sdb(COORDADDR, COORDSVC, SDBUSER, SDBPASSWD);
+        db.close();
+    } catch (error) {
+        println( "Failed to connect sdb, error info: " + error + "(" + getLastErrMsg() + ")" );
+        return false;
+    }
+    return true;
+}
+
+function changeIP() {
+    var db;
+    try {
+        db = new Sdb(COORDADDR, COORDSVC, SDBUSER, SDBPASSWD);
+    } catch (error) {
+        println( "Failed to connect sdb, error info: " + error + "(" + getLastErrMsg() + ")" );
+        return false;
+    }
+
+    try {
+        for (let index = 0; index < IPARRAY.length; index++) {
+            var old_ip = IPARRAY[index].OLD_IP;
+            var new_ip = IPARRAY[index].NEW_IP;
+            println('   Change IP ' + old_ip + ' to ' + new_ip);
+            db.HASysGlobalInfo.HARegistry.update({$set:{"IP":old_ip}},{"IP":new_ip});
+        }
+        db.close();
+    } catch (error) {
+        println( "  Failed to change ip, error info: " + error + "(" + getLastErrMsg() + ")" );
+        return false;
+    }
+
+    return true;
+}
+
+function main() {
+    println("Begin to check Args");
+    if (checkArgs()) {
+        println("done");
+    } else {
+        println("false");
+        return false;
+    }
+
+    println("Begin to change HA IP");
+    if (changeIP()) {
+        println("done");
+    } else {
+        println("false");
+        return false;
+    }
+}
+
+main();
---
+++ readme.txt
@@ -0,0 +1,6 @@
+简易添加网卡工具
+- config.js     配置文件
+- stop.sh       停止本机器所有 SDB 节点和 SQL 实例
+- start.sh      启动本机器所有 SDB 节点和 SQL 实例
+- check.sh      对 SDB 和 SQL 实例进行基本增删改查校验
+- changIP.js    根据配置文件修改实例组注册表的 IP 地址
